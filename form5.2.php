7bc55c022c985e84379c673f98a6c136ce256687
Deka-rahmawati <dekarmhwti@gmail.com>
2026-03-04 10:25:40 +0700

    Add files via upload

---
+++ form5.2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Belanja Warung A</title>
+</head>
+<body>
+
+    <h2>Form Belanja Warung A</h2>
+    <br>
+
+    <form method="POST" action="proses5.2.php">
+
+        Nama Pembeli:<br>
+        <input type="text" name="nama" required><br><br>
+
+        Nama Barang:<br>
+        <input type="text" name="barang" required><br><br>
+
+        Harga:<br>
+        <input type="number" name="harga" required><br><br>
+
+        Jumlah:<br>
+        <input type="number" name="jumlah" required><br><br>
+
+        <button type="submit">Proses Belanja</button>
+
+    </form>
+
+</body>
+</html>
